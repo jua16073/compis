77796cb53941bce9c5e8c03c95cc91b3727df101
Rodrigo Juarez <roderyj@gmail.com>
2020-09-24 12:43:25 -0600

    algunos cambiecitos

--- gui.py
+++ gui.py
@@ -1,9 +1,40 @@
 from flask import Flask, render_template, request, session
+from antlr4 import *
+import antlr4
+from DecafLexer import DecafLexer
+from DecafListener import DecafListener
+from DecafParser import DecafParser
+from antlr4.tree.Trees import Trees
+from DecafVisitor import DecafVisitor
+import tablas as Visitor
+import sistema_de_tipos as tables
+import sys
 
 app  = Flask(__name__)
 app.secret_key = "Triceracop:D"
 
 #session.get no truena
+
+# inicio
+print("todo gucci")
+
+# #program = open('help.txt', 'r+')
+# program = open('final.txt', "r+")
+# #program = open("final_boss.txt", "r+")
+# text = program.read()
+# program.close()
+# text = antlr4.InputStream(text)
+# lexer = DecafLexer(text)
+# stream = CommonTokenStream(lexer)
+# parser = DecafParser(stream)
+# tree = parser.program()
+# printer = DecafListener()
+# walker = ParseTreeWalker()
+# walker.walk(printer, tree)
+# #print(Trees.toStringTree(tree, None, parser))
+# nani = Visitor.MyDecafVisitor()
+# nani.visit(tree)
+
 
 
 @app.route('/')
@@ -15,7 +46,6 @@ def get_code():
     code = request.form["codigo"]
     print(code)
     return render_template("home.html")
-
 
 
 
